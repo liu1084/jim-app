481ef5b881cad35d777a56c5e52c075828c086c2
jim <liu1084@163.com>
2017-02-23 01:59:00 +0800

    init

---
+++ jimapp-api/src/main/java/com/jim/api/BaseController.java
@@ -0,0 +1,14 @@
+package com.jim.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jim on 2017/2/22.
+ * This class is ...
+ */
+@Controller
+public class BaseController {
+	protected final Logger logger = LoggerFactory.getLogger(BaseController.class);
+}
---
+++ jimapp-api/src/main/java/com/jim/api/ebook/controller/EbookController.java
@@ -0,0 +1,12 @@
+package com.jim.api.ebook.controller;
+
+import com.jim.api.BaseController;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jim on 2017/2/22.
+ * This class is ...
+ */
+@Controller
+public class EbookController extends BaseController {
+}
